35893c4fe361522dd6e298fcddd9c130c485e423
DYTye <adityaarrofi806@gmail.com>
2026-07-13 12:26:48 +0700

    finally akhirnya

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,8 +5,8 @@ function home() {
   return (
     <div className="max-w-xl bg-gray-900">
       <div className="backdrop-blur-md bg-[#2a2c2e] fixed top-2 rounded-2xl font-bold text-xl  text-white text-center w-full z-20">
-        <div className="flex items-center justify-center relative">
-          <CiMenuFries className="text-3xl absolute left-5"/>
+        <div className="flex items-center justify-start relative ps-5">
+          <CiMenuFries className="text-3xl"/>
           <div className="p-5 ">Smart Receipt</div>
         </div>
       </div>
